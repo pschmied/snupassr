d03fdbfaddce87c9acab226764c923d7aa677eb8
Peter Schmiedeskamp <peter@thoughtspot.net>
2017-04-12 15:10:36 -0700

    Add exports

--- R/snupassr.R
+++ R/snupassr.R
@@ -1,6 +1,7 @@
 #' Get an entry from lastpass via the CLI. Mostly not used
 #' directly. Mostly.
 #'
+#' @export
 #' @param flags are passed to lpass to specify what should be shown
 #' @param site_entry which entry in lastpass should be shown. Defaults
 #'     to opendata.socrata.com.
@@ -14,6 +15,7 @@ get_lpass <- function(flags, site_entry = "opendata.socrata.com") {
 
 #' Get the user field from a lastpass entry.
 #'
+#' @export
 #' @param site_entry which entry in lastpass should be shown. Defaults
 #'     to opendata.socrata.com.
 get_user <- purrr::partial(get_lpass, flags = "--username")
@@ -21,6 +23,7 @@ get_user <- purrr::partial(get_lpass, flags = "--username")
 
 #' Get the password field from a lastpass entry.
 #'
+#' @export
 #' @param site_entry which entry in lastpass should be shown. Defaults
 #'     to opendata.socrata.com.
 get_pass <- purrr::partial(get_lpass, flags = "--password")
@@ -28,6 +31,7 @@ get_pass <- purrr::partial(get_lpass, flags = "--password")
 
 #' Get the "pubtoken" custom field from a lastpass entry.
 #'
+#' @export
 #' @param site_entry which entry in lastpass should be shown. Defaults
 #'     to opendata.socrata.com.
 get_token <- purrr::partial(get_lpass, flags = "--field=pubtoken")
